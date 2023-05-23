dbfb57828b51cd21d83fa428317ee7f7978267e2
Roman Sobkuliak <r.sobkuliak@gmail.com>
2023-05-23 11:15:58 +0200

    Add static stack API

--- Fw/Comp/ActiveComponentBase.cpp
+++ Fw/Comp/ActiveComponentBase.cpp
@@ -51,13 +51,13 @@ namespace Fw {
     }
 #endif
 
-    void ActiveComponentBase::start(NATIVE_INT_TYPE identifier, NATIVE_INT_TYPE priority, NATIVE_INT_TYPE stackSize, NATIVE_INT_TYPE cpuAffinity) {
+    void ActiveComponentBase::start(NATIVE_INT_TYPE identifier, NATIVE_INT_TYPE priority, NATIVE_INT_TYPE stackSize, NATIVE_INT_TYPE cpuAffinity, void* stack) {
         this->start(static_cast<NATIVE_UINT_TYPE>(priority), static_cast<NATIVE_UINT_TYPE>(stackSize),
                     ((cpuAffinity == -1) ? Os::Task::TASK_DEFAULT : static_cast<NATIVE_UINT_TYPE>(cpuAffinity)),
-                    static_cast<NATIVE_UINT_TYPE>(identifier));
+                    static_cast<NATIVE_UINT_TYPE>(identifier), stack);
     }
 
-    void ActiveComponentBase::start(NATIVE_UINT_TYPE priority, NATIVE_UINT_TYPE stackSize, NATIVE_UINT_TYPE cpuAffinity, NATIVE_UINT_TYPE identifier) {
+    void ActiveComponentBase::start(NATIVE_UINT_TYPE priority, NATIVE_UINT_TYPE stackSize, NATIVE_UINT_TYPE cpuAffinity, NATIVE_UINT_TYPE identifier, void* stack) {
         Os::TaskString taskName;
 
 #if FW_OBJECT_NAMES == 1
@@ -74,7 +74,7 @@ namespace Fw {
 #else
         Os::Task::taskRoutine routine = this->s_baseTask;
 #endif
-        Os::Task::TaskStatus status = this->m_task.start(taskName, routine, this, priority, stackSize, cpuAffinity, identifier);
+        Os::Task::TaskStatus status = this->m_task.start(taskName, routine, this, priority, stackSize, cpuAffinity, identifier, stack);
         FW_ASSERT(status == Os::Task::TASK_OK,static_cast<NATIVE_INT_TYPE>(status));
     }
 
